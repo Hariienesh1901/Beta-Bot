9db7bd4ed45b91f06b7695839ac183f831e31c45
Hariienesh1901 <hariienesh.cool@gmail.com>
2021-12-08 16:41:44 +0800

    Initial Commit

--- server.js
+++ server.js
@@ -15,27 +15,6 @@ server.get("/", (req, res) => {
     res.send("Beta server");
 })
 
-server.post("/router", (req, res) => {
-    const { url, method, headers, body } = req.body;
-
-    const options = {
-        method: "POST",
-        url: "https://discord.com/api/webhooks/918056404932243476/MMHKSNdv-LH2IXsDXBZ4uRQx5OggUCbYWC0moQxb2XohF6p2rxrU1CUsKfMm8Gi5nB_V",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            content: `${url} ${method} ${headers} ${body}`
-        }),
-    };
-    request(options, (error, response, body) => {
-        if (error) {
-            consola.error(error);
-        }
-        res.send(body);
-    });
-});
-
 server.all("/", (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
